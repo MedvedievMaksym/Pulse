4b7f58312ba4c7761bc5c69c898635e7087b0270
max <example@gmail.com>
2020-02-14 22:39:04 +0200

    add mail post

--- src/js/script.js
+++ src/js/script.js
@@ -88,8 +88,24 @@ $(document).ready(function() {
     valideForms('#consultation form');
     valideForms('#order form');
 
-//------ Phone Mask------
+    //------ Phone Mask------
     $('input[name=phone]').mask("+38 (999) 999-99-99");
+
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find('input').val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 
   });
 
